5b9da16334db8a13260adfb2c888e7bde4e7d775
tdk <kamenevms96@gmail.com>
2017-09-22 21:29:56 +0300

    initial commit

---
+++ src/main/java/com/tdkdev/edu/subdlabs/SubdLabsApplication.java
@@ -0,0 +1,12 @@
+package com.tdkdev.edu.subdlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubdLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubdLabsApplication.class, args);
+	}
+}
